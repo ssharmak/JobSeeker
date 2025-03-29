ea420e6c45d9d4af3115c4674a22fbb9a1f0c755
Sankalp <sharathd15899Sharu@gmail.com>
2025-03-29 10:51:39 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import JobSeekerHomepage from './pages/Home_page';
 import LoginPage from './pages/login';
 import EmployeeRegistrationForm from './pages/Employee-reg';
 import InstitutionRegistrationForm from './pages/institution-reg';
+import ProfilePage from './pages/ProfilePage';
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                 <Route path="/login" element={<LoginPage />} />
                 <Route path="/user" element={<EmployeeRegistrationForm />} />
                 <Route path="/inst" element={<InstitutionRegistrationForm />} />
+                <Route path="/ProfilePage" element={<ProfilePage/>}/>
             </Routes>
         </Router>
     );
